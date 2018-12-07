3b63cab8b1fc2d39790fe63fba9c334d679d3a3f
Peter Newton <peter@4sightdesigns.com>
2018-12-07 14:59:21 +0000

    added dependencies

--- server.js
+++ server.js
@@ -3,21 +3,26 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser')
 var cors = require('cors');
 
 var app = express();
 
 // Basic Configuration 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3300;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.URI);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, 'database connection error'));
+db.once("open", function() {
+  console.log("We're connected");
+})
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -33,5 +38,5 @@ app.get("/api/hello", function (req, res) {
 
 
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+  console.log(`Node.js listening on port:${port}`);
 });
