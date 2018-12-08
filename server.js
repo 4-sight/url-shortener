d9aef33fe21ef3d7a816d2f0ba6434da68b674e2
Peter Newton <peter@4sightdesigns.com>
2018-12-08 16:47:50 +0000

    add url validation and db counter

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser')
 var cors = require('cors');
+const dns = require('dns')
 
 var app = express();
 
@@ -30,12 +31,68 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+const counterSchema = new mongoose.Schema({
+  name: String,
+  index: {type: Number, default: 1},
+})
 
+const Counter = mongoose.model("counter", counterSchema)
+
+const done = (data) => {console.log(data.index)}
+
+const findAndUpdateCounter = function(done) {
+  Counter.findOneAndUpdate(
+    {name: "miniCounter"},
+    {$inc: {index: 1}},
+    {upsert: true, setDefaultsOnInsert: true},
+    function(err, data) {
+      err
+      ? console.log(err)
+      : done(data)
+    })
+}
+
+const miniSchema = new mongoose.Schema({
+  url: String,
+  miniId: Number
+})
+
+const Mini = mongoose.model("mini", miniSchema)
+
+const createAndSaveMini = function(url) {
+  let counter = findAndUpdateCounter()
+  
+  const entry = new Mini({
+    url: url
+  })
+  return entry.save(function(err, data) {
+    err
+    ? console.log(err)
+    : data
+  })
+}
+
+const validateUrl = (url, done) => {
+
+  const trimmed = url.replace(/http[s]?:\/\//, "")
+
+  dns.lookup(trimmed, function(err, data) {
+    err
+    ? done(false)
+    : done(true)
+  })
+}
+
+const next = (val) => {
+  val
+  ? console.log("valid")
+  : console.log("invalid")
+}
+//------------------------------------------------------------
+app.post('/api/shorturl/new', function(req, res) {
+  validateUrl(req.body.url, next)
+  res.json(req.body.url)
+})
 
 app.listen(port, function () {
   console.log(`Node.js listening on port:${port}`);
